09380df95b6a102ac2d9a88df49d04ab9fdb729a
Kevin Yang <kuan_yang@mfe.berkeley.edu>
2014-01-01 12:09:22 -0800

    Add space around skills in many-to-many, fixed bug in showing empty field in quick reference for course detail

--- many_to_many.py
+++ many_to_many.py
@@ -6,15 +6,16 @@ if __name__ == '__main__':
     skills = SkillsList.objects.filter(pk__gt = 8505)
 
     for skill in skills:
+        skill_name = " " + skill.skill + " "
         for course in CoursesList.objects.all():
             # The skill keyword from SkillsList appears in 
             # the description or title field in course
-            if skill.skill in course.coursetitle or skill.skill in course.coursedescription:
+            if skill_name in course.coursetitle or skill.skill in course.coursedescription:
                course.skillsLists.add(skill)
 
 
         for job in JobsList.objects.all():
-            if skill.skill in job.skills:
+            if skill_name in job.skills:
                 job.skillsLists.add(skill)
 
 
