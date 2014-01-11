9d9610bf0b80a9fdacba0cb5e1ab4ee323e83a7e
youngwide <yksubscribe@gmail.com>
2014-01-11 19:37:00 +0000

    Add database setting for server

--- ejub/settings.py
+++ ejub/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+# STATIC_ROOT = os.path.join(BASE_DIR, 'ej/static').replace('\\','/')
+STATIC_ROOT = os.path.join(BASE_DIR, '../../ejub_static').replace('\\','/')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
@@ -63,8 +65,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'test_ej',
-        'USER': 'root',
-        'PASSWORD': 'root',
+        'USER': 'youngwide',
+        'PASSWORD': '198989',
         'HOST': 'localhost',
         'PORT': '3306',
     }
@@ -87,4 +89,4 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'http://www.ejub.us/ej/static/'
