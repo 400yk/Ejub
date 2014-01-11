62b3f9ba3bb00d1722d54acec248542e84f63612
Kevin Yang <kuan_yang@mfe.berkeley.edu>
2014-01-11 11:53:43 -0800

    Add home redirect, update url to /berkeley/

--- ejub/urls.py
+++ ejub/urls.py
@@ -1,6 +1,6 @@
 import settings
 from django.conf.urls import patterns, include, url
-
+from django.views.generic import RedirectView
 from django.contrib import admin
 admin.autodiscover()
 
@@ -8,8 +8,8 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'ejub.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'^$', RedirectView.as_view(url='/berkeley/')),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^ej/', include('ej.urls', namespace = 'ej')),
+    url(r'^berkeley/', include('ej.urls', namespace = 'ej')),
 )
 
